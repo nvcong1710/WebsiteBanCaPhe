e1377e5c01d47063fbe149999ce136f5a582115a
admin <cong171002@gmail.com>
2023-11-07 10:39:29 +0700

    create controller ProductByCategory and ui

---
+++ Controllers/ProductByCategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebsiteBanCaPhe.Data;
+using WebsiteBanCaPhe.Models;
+
+namespace WebsiteBanCaPhe.Controllers
+{
+	public class ProductByCategoryController : Controller
+	{
+		private readonly WebsiteBanCaPheContext _context;
+
+		public ProductByCategoryController(WebsiteBanCaPheContext context)
+		{
+			_context = context;
+		}
+		public IActionResult Index(int? id)
+		{
+			var productByCategory = _context.Product
+				.Where(x => x.CategoryId == id);
+			return View(productByCategory.ToList());
+		}
+	}
+}
