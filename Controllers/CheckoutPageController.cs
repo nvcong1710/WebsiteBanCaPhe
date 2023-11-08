df6fd66a741a49ecb4f9a00391cffd2bce6cfd15
Nguyễn Viết Công <91941271+nvcong1710@users.noreply.github.com>
2023-11-08 08:58:38 +0700

    fix checkout

--- Controllers/CheckoutPageController.cs
+++ Controllers/CheckoutPageController.cs
@@ -1,32 +1,66 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 using WebsiteBanCaPhe.Data;
 using WebsiteBanCaPhe.Models;
 
 namespace WebsiteBanCaPhe.Controllers
 {
-	public class CheckoutPageController : Controller
-	{
-		private readonly WebsiteBanCaPheContext _context;
+    public class CheckoutPageController : Controller
+    {
+        private readonly WebsiteBanCaPheContext _context;
 
-		public CheckoutPageController(WebsiteBanCaPheContext context)
-		{
-			_context = context;
-		}
-		public async Task<IActionResult> Index()
-		{
-			var accountId = HttpContext.Session.GetString("AccountId");
-			var cart = await _context.Cart.FirstOrDefaultAsync(c => c.AccountId.ToString() == accountId);
-			var cartId = cart.CartId;
-			var websiteBanCaPheContext = _context.CartDetail
-				.Include(c => c.Cart)
-				.Include(c => c.Product)
-				.Where(c => c.CartId == cartId);
-			var cartTotalValue = websiteBanCaPheContext.Sum(c => c.TotalPrice);
+        public CheckoutPageController(WebsiteBanCaPheContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var accountId = HttpContext.Session.GetString("AccountId");
+            var cart = await _context.Cart.FirstOrDefaultAsync(c => c.AccountId.ToString() == accountId);
+            var cartId = cart.CartId;
+            var websiteBanCaPheContext = _context.CartDetail
+                .Include(c => c.Cart)
+                .Include(c => c.Product)
+                .Where(c => c.CartId == cartId);
+            var cartTotalValue = websiteBanCaPheContext.Sum(c => c.TotalPrice);
 
-			ViewBag.CartTotalValue = cartTotalValue;
-			return View(await websiteBanCaPheContext.ToListAsync());
-		}
-	}
+            ViewBag.CartTotalValue = cartTotalValue;
+            ViewBag.CartDetails = await websiteBanCaPheContext.ToListAsync();
+            UserOrder userOrder;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index([Bind("OrderDate,ReceiverName,PhoneNumber,Address,PaymentMethod,Note,ShippingFee,TotalValue,IsDone,AccountId")] UserOrder userOrder)
+        {
+            var accountId = HttpContext.Session.GetString("AccountId");
+            var cart = await _context.Cart.FirstOrDefaultAsync(c => c.AccountId.ToString() == accountId);
+            var cartId = cart.CartId;
+            var websiteBanCaPheContext = _context.CartDetail
+                .Include(c => c.Cart)
+                .Include(c => c.Product)
+                .Where(c => c.CartId == cartId);
+            var cartTotalValue = websiteBanCaPheContext.Sum(c => c.TotalPrice);
+
+            ViewBag.CartTotalValue = cartTotalValue;
+            ViewBag.CartDetails = await websiteBanCaPheContext.ToListAsync();
+            if (ModelState.IsValid)
+            {
+                userOrder.OrderDate = DateTime.Now;
+                userOrder.ShippingFee = 0;
+                userOrder.IsDone = false;
+                userOrder.AccountId = cart.AccountId;
+
+
+                _context.Add(userOrder);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AccountId"] = new SelectList(_context.Account, "AccountId", "FullName", userOrder.AccountId);
+            return View(userOrder);
+        }
+    }
 }
