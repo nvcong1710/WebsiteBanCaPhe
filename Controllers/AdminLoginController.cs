fc3bee9cd65890798aa0e65e9c133f86ec548def
Nguyễn Viết Công <91941271+nvcong1710@users.noreply.github.com>
2023-12-24 12:05:05 +0700

    add admin login

---
+++ Controllers/AdminLoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebsiteBanCaPhe.Controllers
+{
+    public class AdminLoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
